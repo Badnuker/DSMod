fc7898a0174ac283149562ce8dd81b92e43a71c3
Badnuker <badnuker@126.com>
2025-03-14 09:11:28 +0800

    初始提交

---
+++ src/main/java/net/badnuker/DefinatelyStupidModClient.java
@@ -0,0 +1,10 @@
+package net.badnuker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DefinatelyStupidModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
