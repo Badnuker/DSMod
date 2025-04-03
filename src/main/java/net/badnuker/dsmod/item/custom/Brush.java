b3fe70285250f12463e54be6169411bf7cf16c90
Badnuker <badnuker@126.com>
2025-04-03 16:44:16 +0800

    重构部分代码

--- src/main/java/net/badnuker/dsmod/item/custom/Brush.java
+++ src/main/java/net/badnuker/dsmod/item/custom/Brush.java
@@ -1,8 +1,6 @@
 package net.badnuker.dsmod.item.custom;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
@@ -19,10 +17,5 @@ public class Brush extends Item {
         BlockPos pos = context.getBlockPos();
         world.breakBlock(pos, false); // 破坏方块但不生成掉落物
         return ActionResult.SUCCESS;
-    }
-
-    @Override
-    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-        return false; // 无法对生物造成伤害
     }
 }
